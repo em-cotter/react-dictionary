59140769547832bea08d902f3562a7e46c3c5342
em-cotter <ecotter22@gmail.com>
2022-04-11 08:56:37 +0200

    Included search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container"></div>
       <header className="App-header">
-        hello
+        Dictionray
       </header>
+      <main>
+        <Dictionary />
+      </main>
     </div>
   );
 }
